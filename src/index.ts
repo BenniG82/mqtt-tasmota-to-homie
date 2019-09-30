d6f57ed31a40e5ddc98ca28be8028dd45b8e4174
Benjamin Gniza <benjamin.gniza@bridging-it.de>
2019-09-30 12:07:37 +0200

    Moved app start to a file with meaningful filename

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { SonoffTopicListener } from './sonoff-topic-listener';
+
+/**
+ * Before you start this file, please take a look at the settings.ts and modify them accordingly.
+ */
+SonoffTopicListener.start();
